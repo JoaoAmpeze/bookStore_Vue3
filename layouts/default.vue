616f7ea3c29ec11a54562ffebc1502065f7fe20d
Joaoampeze <ampeze@unochapeco.edu.br>
2024-06-04 18:01:13 -0300

    sucess

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout>
+    <v-row style="background-color: #007a7c; height: 70px;">
+      <v-col>
+        aaa
+      </v-col>
+      <v-col>
+        aa
+      </v-col>
+    </v-row>
+
+  </v-layout>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <body style="background-color: rgb(237, 235, 235); height:100dvh;">
-    <h1>ss</h1>
+    
   </body>
 </template>
