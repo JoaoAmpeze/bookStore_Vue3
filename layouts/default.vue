031979a4f612150e9234b1bc829a16250592afd4
Joaoampeze <ampeze@unochapeco.edu.br>
2024-06-05 18:08:50 -0300

    sucess

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,56 @@
 <template>
   <v-layout>
-    <v-row style="background-color: #007a7c; height: 70px;">
-      <v-col>
-        aaa
-      </v-col>
-      <v-col>
-        aa
+    <v-row style="background-color: #005859; height: 80px; width: 3000px">
+      <v-col md="2"> </v-col>
+      <v-col class="mt-2">
+        <span
+          >Compre já usando o Cupom LEVA10 paga garantir 10% de desconto na sua primeira compra!</span
+        >
       </v-col>
     </v-row>
-
   </v-layout>
+  <v-layout>
+    <v-row variant="elevated" style="background-color: #007a7c; height: 70px; width: 100px">
+      <v-col md="1"></v-col>
+      <v-col md="2">
+        <h2 class="ml-15 mt-1 font-weight-bold">LIVRERO</h2>
+      </v-col>
+      <v-col class="ml-n12 mt-1" md="1">
+        <v-menu transition="slide-y-transition">
+          <template v-slot:activator="{ props }">
+            <v-btn variant="tonal" height="40" color="#007a7c" v-bind="props">
+              <v-icon size="x-large" color="white ">mdi-menu</v-icon>
+            </v-btn>
+          </template>
+          <v-list height="600" width="600" style="background-color: aliceblue">
+            <v-btn>a</v-btn>
+          </v-list>
+        </v-menu>
+      </v-col>
+      <v-col class="mt-1 ml-n15">
+        <v-card height="40" width="400" style="background-color: white">
+          <v-text-field
+            style="color: black"
+            class="mt-n2"
+            label="Pesquisa"
+            prepend-inner-icon="mdi-magnify"
+            hide-details
+            single-line
+            width="400"
+          />
+        </v-card>
+      </v-col>
+      <v-col md="5" class="mt-">
+        <v-btn  size="large" class="font-weight-bold" style="background-color: #007a7c" variant="text" prepend-icon="mdi-heart-outline">
+        Minha lista
+        </v-btn>
+        <v-btn size="large" class="ml-1 font-weight-bold" style="background-color: #007a7c" variant="text" prepend-icon="mdi-account-outline">
+          Olá, ...
+        </v-btn>
+        <v-btn  size="large" class="ml-1 font-weight-bold" style="background-color: #007a7c" variant="text" prepend-icon="mdi-cart-outline">
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-layout>
+    <slot/>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <body style="background-color: rgb(237, 235, 235); height:100dvh;">
-    
+  <body style="background-color: white">
+    <v-row>
+      <v-col class="mt-3">
+        <v-carousel  height="300" :show-arrows="false" :cycle="true" interval="3000">
+          <v-carousel-item
+            src="https://d2da0u06q4mna7.cloudfront.net/highlights/namorados-full-banner-desk-.png"
+            cover
+          ></v-carousel-item>
+
+          <v-carousel-item
+            src="https://d2da0u06q4mna7.cloudfront.net/highlights/feirao-de-livros-23-home-desk.jpg"
+            cover
+          ></v-carousel-item>
+
+          <v-carousel-item
+            src="https://d2da0u06q4mna7.cloudfront.net/highlights/Independentes+Banner+Home+Desktop+-+1920x350px.png"
+            cover
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    <v-row  style="background-color: rgb(141, 141, 141); height: 500px;">
+      <v-col md="1">
+
+      </v-col>
+      <v-col md="6" class="mt-5">
+        <h2> Livros mas vendidos</h2>
+      </v-col>
+    </v-row>
+    <v-row class="mt-6" style="background-color: rgb(141, 141, 141); height: 500px;">
+      <v-col>
+      </v-col>
+    </v-row>
+    <v-row class="mt-6" style="background-color: rgb(141, 141, 141); height: 500px;">
+      <v-col>
+      </v-col>
+    </v-row>
   </body>
 </template>
